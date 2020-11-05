be599d580ce6b66a52e08778b60239b79c368a2c
Harrison Higgins <harryrhiggins@gmail.com>
2020-11-05 16:37:27 -0700

    Finished phase 4

--- path_finders.py
+++ path_finders.py
@@ -6,6 +6,7 @@ class KnightPathFinder:
         self._root = Node(position)
         self._considered_positions = set((position,))
 
+
     def get_valid_moves(self, pos):
         deltas = ((1, 2), (1, -2), (2, 1), (2, -1),
                   (-1, 2), (-1, -2), (-2, 1), (-2, -1))
@@ -15,6 +16,7 @@ class KnightPathFinder:
                  and pos[1] + y >= 0 and pos[1] + y < 8]
         return moves
 
+
     def new_move_positions(self, pos):
         new_moves = [position for position in self.get_valid_moves(pos)
                      if position not in self._considered_positions]
@@ -23,31 +25,33 @@ class KnightPathFinder:
 
 
     def build_move_tree(self):
-      nodes = [self._root]
-      while nodes:
-        node = nodes.pop(0)
-        moves = self.new_move_positions(node.value)
-        children = [Node(pos) for pos in moves]
-        for child in children:
-          node.add_child(child)
-        nodes.extend(children)
+        nodes = [self._root]
+        while nodes:
+            node = nodes.pop(0)
+            moves = self.new_move_positions(node.value)
+            children = [Node(pos) for pos in moves]
+            for child in children:
+                node.add_child(child)
+            nodes.extend(children)
+
 
     def find_path(self, end_position):
-      node = self._root.depth_search(end_position)
-      return self.trace_to_root(node)
+        node = self._root.depth_search(end_position)
+        return self.trace_to_root(node)
+
 
     def trace_to_root(self, end_node):
-      node = end_node
-      path = [end_node.value]
-      while node.parent:
-        node = node.parent
-        path.append(node.value)
-      return list(reversed(path))
+        node = end_node
+        path = [end_node.value]
+        while node.parent:
+            node = node.parent
+            path.append(node.value)
+        return list(reversed(path))
 
 
 finder = KnightPathFinder((0, 0))
 finder.build_move_tree()
 print(finder.find_path((2, 1))) # => [(0, 0), (2, 1)]
-print(finder.find_path((3, 3))) # => [(0, 0), (2, 1), (3, 3)]
-# print(finder.find_path((6, 2))) # => [(0, 0), (1, 2), (2, 4), (4, 3), (6, 2)]
-# print(finder.find_path((7, 6))) # => [(0, 0), (1, 2), (2, 4), (4, 3), (5, 5), (7, 6)]
+print(finder.find_path((3, 3)))  # => [(0, 0), (2, 1), (3, 3)]
+print(finder.find_path((6, 2))) # => [(0, 0), (1, 2), (2, 4), (4, 3), (6, 2)]
+print(finder.find_path((7, 6))) # => [(0, 0), (1, 2), (2, 4), (4, 3), (5, 5), (7, 6)]
--- tree.py
+++ tree.py
@@ -4,61 +4,64 @@ class Node:
         self._parent = None
         self._children = []
 
+
     @property
     def value(self):
         return self._value
 
+
     @property
     def children(self):
         return self._children
+
 
     def add_child(self, node):
         if node not in self.children:
             self.children.append(node)
             node.parent = self
 
+
     def remove_child(self, node):
         self.children.remove(node)
         node.parent = None
+
 
     @property
     def parent(self):
         return self._parent
 
+
     @parent.setter
     def parent(self, node):
         if node == None:
-          self._parent = None
-          return
+            self._parent = None
+            return
 
         if node == self.parent:
-          return
+            return
         if self.parent:
             self.parent.remove_child(self)
         self._parent = node
         node.add_child(self)
 
 
-    def depth_search(self, value, visited=set()):
-      # print(self.value)
-      if self == None or self in visited:
-        return None
-      if self.value == value:
-          return self
-      else:
-        visited.add(self)
-        for child in self.children:
-          found = child.depth_search(value, visited)
-          if found:
-            print(found.value)
-            return found
+    def depth_search(self, value):
+        if self == None:
+            return None
+        if self.value == value:
+            return self
+        else:
+            for child in self.children:
+                found = child.depth_search(value)
+                if found:
+                    return found
 
 
     def breadth_search(self, value, queue=[]):
         if self == None:
-          return None
+            return None
         if self.value == value:
             return self
         else:
-          queue.extend(self.children)
-          return queue.pop(0).breadth_search(value, queue)
+            queue.extend(self.children)
+            return queue.pop(0).breadth_search(value, queue)
